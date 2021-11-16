640d93131e8d4234970d27a3b4d5f6200f3e8745
adregis <adregis2000@yahoo.com>
2021-11-16 17:00:17 -0300

    atualiza terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,14 @@ terraform {
     }
 }
 
-
 resource "digitalocean_droplet" "web" {
     image   = "ubuntu-20-04-x64"
-    name    = "web-2"
+    name    = "web-5"
     region  = "nyc1"
     size    = "s-1vcpu-1gb"
 }
+
+output "ip_do_droplet" {
+    value   = digitalocean_droplet.web.ipv4_address
+}
+ 
