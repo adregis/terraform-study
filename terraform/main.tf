20c1b019a7020cf27a0c76bcb0934e7bf25ec723
adregis <adregis2000@yahoo.com>
2021-11-17 14:27:08 -0300

    aula-3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,25 @@
 terraform {
-    required_providers {
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-        }
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
     }
+  }
 }
 
 resource "digitalocean_droplet" "web" {
-    image   = "ubuntu-20-04-x64"
-    name    = "web-5"
-    region  = "nyc1"
-    size    = "s-1vcpu-1gb"
-    ssh_keys    = ["8b:02:a2:ab:cb:e6:eb:2d:44:41:09:fa:b6:ea:68:3b"]
+  image    = "ubuntu-20-04-x64"
+  name     = "web"
+  region   = "nyc1"
+  size     = "s-1vcpu-1gb"
+  ssh_keys = ["8b:02:a2:ab:cb:e6:eb:2d:44:41:09:fa:b6:ea:68:3b", "33:f3:1c:0d:cb:2c:ce:2e:f7:0a:7a:bf:f1:3a:83:fe"]
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes = [
+      ssh_keys,
+    ]
+  }
 }
-
 output "ip_do_droplet" {
-    value = digitalocean_droplet.web.ipv4_address
+  value = digitalocean_droplet.web.ipv4_address
 }
  
