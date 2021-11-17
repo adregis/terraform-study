c169550f49b31c52c71e2f432f443148b47b9336
adregis <adregis2000@yahoo.com>
2021-11-17 14:26:50 -0300

    aula-2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,12 @@
 variable "digitalocean_token" {}
 
 provider "digitalocean" {
-token = "${var.digitalocean_token}"
+  token = var.digitalocean_token
+}
+
+variable "droplet_image" {
+    type = string
+    description = "imagem base do projeto"
+    default = "ubuntu-20-04"
+
 }
