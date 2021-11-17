c051a531db45cff33fa0a5023b17f67b327a2d44
adregis <adregis2000@yahoo.com>
2021-11-17 17:03:40 -0300

    config acesso

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,21 @@
 terraform {
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
 }
 
 provider "digitalocean" {
-    token = var.do_token
+  token = var.do_token
 }
 
 resource "digitalocean_droplet" "web" {
-  image    = "ubuntu-20-04-x64"
+  image    = var.droplet_image
   name     = "web"
-  region   = "nyc1"
-  size     = "s-1vcpu-1gb"
+  region   = var.droplet_region
+  size     = var.droplet_size
   ssh_keys = ["8b:02:a2:ab:cb:e6:eb:2d:44:41:09:fa:b6:ea:68:3b", "33:f3:1c:0d:cb:2c:ce:2e:f7:0a:7a:bf:f1:3a:83:fe"]
   lifecycle {
     create_before_destroy = true
