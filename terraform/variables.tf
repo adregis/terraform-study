f7cbbc35e228cf116795defb05b15804e25e05fa
adregis <adregis2000@yahoo.com>
2021-11-17 17:07:25 -0300

    gitignore

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,24 @@
 variable "do_token" {
-  type  = string  
+  type        = string
   description = "Token para acesso aos recursos Digital Ocean"
 }
 
 variable "droplet_image" {
-    type = string
-    description = "imagem base do projeto"
-    default = "ubuntu-20-04"
+  type        = string
+  description = "imagem base do projeto"
+  default     = "ubuntu-18-04-x64"
 
 }
+
+variable "droplet_region" {
+  type        = string
+  description = "Regiao onde os droplets serão criados"
+  default     = "ams3"
+}
+
+variable "droplet_size" {
+  type        = string
+  description = "Tamanho do droplet"
+  default     = "s-1vcpu-1gb"
+}
+
