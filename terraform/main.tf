4b2e46ca030e57fd04e7c4e2083c742beafa9d82
adregis <adregis2000@yahoo.com>
2021-11-23 16:57:22 -0300

    config firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,3 +80,27 @@ resource "digitalocean_database_user" "username" {
   cluster_id = digitalocean_database_cluster.postgres.id
   name       = var.database_username
 }
+
+resource "digitalocean_firewall" "web" {
+  name = "only-22-80-and-443"
+
+  droplet_ids = [digitalocean_droplet.web.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
